b386a2f17dba9fac38be48e2ca6cfa983169a58b
lauraol <lauraoliveirape@gmail.com>
2021-07-22 16:28:34 -0300

    adicionando section de depoimentos e contato + libs swipe e scrollreveal

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*menu mobile: abre e fecha o menu quando clicar no icone */
+/* menu mobile: abre e fecha o menu quando clicar no icone */
 const nav = document.querySelector("#header nav");
 const toggle = document.querySelectorAll("nav .toggle");
 
@@ -8,7 +8,7 @@ for (const element of toggle) {
   });
 }
 
-/*fechar menu quando clicar e algum item do menu */
+/* fechar menu quando clicar e algum item do menu */
 const links = document.querySelectorAll("nav ul li a");
 
 for (const link of links) {
@@ -17,7 +17,7 @@ for (const link of links) {
   });
 }
 
-/*colocar uma sombrinha no header da página quando rolar um scroll */
+/* colocar uma sombrinha no header da página quando rolar um scroll */
 const header = document.querySelector("#header");
 const navHeight = header.offsetHeight;
 
@@ -30,3 +30,32 @@ window.addEventListener("scroll", function () {
     header.classList.remove("scroll");
   }
 });
+
+/* Slider da section de depoimentos */
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+/* Animação que mostra elementos conforme o usuários vai dando scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
+
+scrollReveal.reveal(
+  `
+#home .text, #home .image,
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials,
+#contact .text, #contact .liks
+`,
+  { interval: 100 }
+);
